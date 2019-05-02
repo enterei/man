209265ef2b23e2a92fb29056e704438486c7ab2d
Oliver Lell <a01303327@unet.univie.ac.at>
2019-05-02 13:07:01 +0200

    d
    d

--- paths.js
+++ paths.js
@@ -3,7 +3,7 @@ var path0 = new Path(-1.35,-0.44999999999999,-0.0001,0.00001,true,0);
 var path1= new Path(-0.45,-0.449, -0.3999999165534973,.3999999165534973,false,1);
 var path2 = new Path(-1.35,-0.4499999999,-0.4,-0.389999999999999999999,false,2); 
 var path3 = new Path(-1.35,-1.34,-0.3999999165534973,+0.399999999999999999999,false,3); 
-var path4 = new Path(-1.35,-1.0299994945526123,-0.3999999165534973,-0.38,false,3); 
+var path4 = new Path(-1.35,-1.0299994945526123,0.4,  0.38,false,4); 
 
 path0.neighbors.push(path0);
 path0.neighbors.push(path1);
@@ -17,9 +17,14 @@ path2.neighbors.push(path3);
 path3.neighbors.push(path3);
 path3.neighbors.push(path2);
 path3.neighbors.push(path0);
+path3.neighbors.push(path4);
+path4.neighbors.push(path4);
+path4.neighbors.push(path3);
+
 var paths=[];
 paths.push(path0);
 paths.push(path1);
 paths.push(path2);
 paths.push(path3);
+paths.push(path4);
 console.log(paths);
